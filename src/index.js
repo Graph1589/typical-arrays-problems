cd501d7fc9a3c853dad2b6941f76422c3f2f9c40
graph1589 <graph1589@gmail.com>
2021-03-08 19:29:58 +0300

    feat: implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return array.reduce((m, n) => (m < n ? m : n), array[0]);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return array.reduce((m, n) => (m > n ? m : n), array[0]);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return array.reduce((acc, num) => (acc + num), 0) / array.length;
 }
